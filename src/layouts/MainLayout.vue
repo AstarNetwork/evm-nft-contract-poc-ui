03609373e54fb63f2576bedf4a3bf80b204c5e65
Gregory Luneau <gregory.luneau@gmail.com>
2023-04-18 05:40:50 -0500

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="lhh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> ShidenCrust Singular Minter dApp </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {},
+
+  setup() {
+    return {};
+  },
+});
+</script>
